99d1e12d5b43eb9b84a832940a6b974393de8627
pj潘婧 <pan.jing@ctrip.com>
2017-04-07 11:26:41 +0800

    add TMS folder

---
+++ TMS/Test/src/DBConnection.java
@@ -0,0 +1,36 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DBConnection {
+	
+	private static Connection conn = null ;
+	
+	public Connection dbconn() {
+		String url = "jdbc:mysql://localhost:3306/book?useUnicode=true&characterEncoding=UTF-8";
+		String user = "root";
+		String pwd = "";
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			System.out.println("类加载失败...");
+		}
+		
+		try {
+			conn = DriverManager.getConnection(url, user, pwd);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("数据库连接失败...");
+		}		
+		
+		return conn;
+	}
+	
+	
+	
+
+}
---
+++ TMS/Test/src/ListForm.java
@@ -0,0 +1,96 @@
+
+public class ListForm {
+	private String projectId;
+	private String taskId;
+	private String title;
+	private String deliverNo;
+	private String developer;
+	private String tester;
+	private String applicationName;
+	private String content;
+	private String comments;
+	private String createUser;
+	private String updateUser;
+	private String CreateTime;
+	private String UpdateTime;
+	
+	public String getProjectId() {
+		return projectId;
+	}
+	public void setProjectId(String projectId) {
+		this.projectId = projectId;
+	}
+	public String getTaskId() {
+		return taskId;
+	}
+	public void setTaskId(String taskId) {
+		this.taskId = taskId;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDeliverNo() {
+		return deliverNo;
+	}
+	public void setDeliverNo(String deliverNo) {
+		this.deliverNo = deliverNo;
+	}
+	public String getDeveloper() {
+		return developer;
+	}
+	public void setDeveloper(String developer) {
+		this.developer = developer;
+	}
+	public String getTester() {
+		return tester;
+	}
+	public void setTester(String tester) {
+		this.tester = tester;
+	}
+	public String getApplicationName() {
+		return applicationName;
+	}
+	public void setApplicationName(String applicationName) {
+		this.applicationName = applicationName;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getComments() {
+		return comments;
+	}
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+	public String getCreateUser() {
+		return createUser;
+	}
+	public void setCreateUser(String createUser) {
+		this.createUser = createUser;
+	}
+	public String getUpdateUser() {
+		return updateUser;
+	}
+	public void setUpdateUser(String updateUser) {
+		this.updateUser = updateUser;
+	}
+	public String getCreateTime() {
+		return CreateTime;
+	}
+	public void setCreateTime(String createTime) {
+		CreateTime = createTime;
+	}
+	public String getUpdateTime() {
+		return UpdateTime;
+	}
+	public void setUpdateTime(String updateTime) {
+		UpdateTime = updateTime;
+	}
+
+}
